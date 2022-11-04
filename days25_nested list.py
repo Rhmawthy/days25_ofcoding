b906a2336c3367f7bec556320ec8b04e11ba32d5
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-11-04 06:33:03 -0700

    Add files via upload

---
+++ days25_nested list.py
@@ -0,0 +1,22 @@
+
+#mengambil data list
+data_1 = [1,2]
+data_2 = [7,6]
+dt= [data_1, data_2]
+print (dt)
+data = dt[0]
+print (data)
+
+#cara megambil data dari nested list
+data_1 = [1,2]
+data_2 = [7,6]
+dt= [data_1, data_2]
+data = dt[0][1]
+print (data)
+
+
+
+
+
+
+    
